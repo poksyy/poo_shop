e425ffd62293be2f040b0bc28b7b017b7aeed61c
Pau Carrera <poksy2002@gmail.com>
2024-05-17 00:10:20 +0200

    style(menu): add visual buttons to menu

--- src/view/ShopView.java
+++ src/view/ShopView.java
@@ -20,12 +20,20 @@ import main.Shop;
 import model.Amount;
 
 public class ShopView extends JFrame implements ActionListener, KeyListener {
-	private JButton showCashButton;
-	private JButton addProductButton;
-	private JButton addStockButton;
-	private JButton showInventoryButton;
-	private JButton deleteProductButton;
-	private JButton exitButton;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JButton btnShowCash;
+	private JButton btnAddProduct;
+	private JButton btnAddStock;
+	private JButton btnSetExpired;
+	private JButton btnShowInventory;
+	private JButton btnMakeSale;
+	private JButton btnShowSale;
+	private JButton btnDeleteProduct;
+	private JButton btnShowTotalSale;
+	private JButton btnExit;
 	private Shop shop;
 
 	public static void main(String[] args) {
@@ -57,17 +65,25 @@ public class ShopView extends JFrame implements ActionListener, KeyListener {
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == showCashButton) {
+		if (e.getSource() == btnShowCash) {
 			openCashView();
-		} else if (e.getSource() == addProductButton) {
+		} else if (e.getSource() == btnAddProduct) {
 			openProductView(Constants.ADD_PRODUCT);
-		} else if (e.getSource() == addStockButton) {
+		} else if (e.getSource() == btnAddStock) {
 			openProductView(Constants.ADD_PRODUCT_STOCK);
-		} else if (e.getSource() == showInventoryButton) {
+		} else if (e.getSource() == btnSetExpired) {
+			openExpiredView();
+		} else if (e.getSource() == btnShowInventory) {
 			openInventoryView();
-		} else if (e.getSource() == deleteProductButton) {
+		} else if (e.getSource() == btnMakeSale) {
+			openSaleView();
+		} else if (e.getSource() == btnShowSale) {
+			openSaleRecords();
+		} else if (e.getSource() == btnDeleteProduct) {
 			openProductView(Constants.DELETE_PRODUCT);
-		} else if (e.getSource() == exitButton) {
+		} else if (e.getSource() == btnShowTotalSale) {
+			openTotalSale();
+		} else if (e.getSource() == btnExit) {
 			exitOption();
 		}
 	}
@@ -99,6 +115,11 @@ public class ShopView extends JFrame implements ActionListener, KeyListener {
 		productView.setVisible(true);
 	}
 
+	private void openExpiredView() {
+		// TODO Auto-generated method stub
+
+	}
+
 	/**
 	 * Opens the InventoryView window at a calculated position.
 	 */
@@ -108,6 +129,21 @@ public class ShopView extends JFrame implements ActionListener, KeyListener {
 		InventoryView inventoryView = new InventoryView(shop);
 		inventoryView.setLocation(cashViewPosition);
 		inventoryView.setVisible(true);
+	}
+
+	private void openSaleView() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void openSaleRecords() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void openTotalSale() {
+		// TODO Auto-generated method stub
+
 	}
 
 	/**
@@ -128,56 +164,73 @@ public class ShopView extends JFrame implements ActionListener, KeyListener {
 		setLocation(calculateXPosition(), calculateYPosition());
 		shop = new Shop();
 
-		// Text fields and buttons
-		showCashButton = new JButton("1. Show cash");
-		showCashButton.setFont(new Font("Arial", Font.BOLD, 10));
-		addProductButton = new JButton("2. Add product");
-		addProductButton.setFont(new Font("Arial", Font.BOLD, 10));
-		addStockButton = new JButton("3. Add stock");
-		addStockButton.setFont(new Font("Arial", Font.BOLD, 10));
-		showInventoryButton = new JButton("5. Show inventory");
-		showInventoryButton.setFont(new Font("Arial", Font.BOLD, 10));
-		deleteProductButton = new JButton("9. Delete product");
-		deleteProductButton.setFont(new Font("Arial", Font.BOLD, 10));
-		exitButton = new JButton("10. Exit");
-		exitButton.setFont(new Font("Arial", Font.BOLD, 10));
+		// text fields and buttons
+		btnShowCash = new JButton("1. Show cash");
+		btnShowCash.setFont(new Font("Arial", Font.BOLD, 10));
+		btnAddProduct = new JButton("2. Add product");
+		btnAddProduct.setFont(new Font("Arial", Font.BOLD, 10));
+		btnAddStock = new JButton("3. Add stock");
+		btnAddStock.setFont(new Font("Arial", Font.BOLD, 10));
+		btnSetExpired = new JButton("4. Set expired");
+		btnSetExpired.setFont(new Font("Arial", Font.BOLD, 10));
+		btnShowInventory = new JButton("5. Show inventory");
+		btnShowInventory.setFont(new Font("Arial", Font.BOLD, 10));
+		btnMakeSale = new JButton("6. Make sales");
+		btnMakeSale.setFont(new Font("Arial", Font.BOLD, 10));
+		btnShowSale = new JButton("7. Sales records");
+		btnShowSale.setFont(new Font("Arial", Font.BOLD, 10));
+		btnDeleteProduct = new JButton("8. Delete product");
+		btnDeleteProduct.setFont(new Font("Arial", Font.BOLD, 10));
+		btnShowTotalSale = new JButton("9. Total sales");
+		btnShowTotalSale.setFont(new Font("Arial", Font.BOLD, 10));
+		btnExit = new JButton("10. Exit");
+		btnExit.setFont(new Font("Arial", Font.BOLD, 10));
 
-		// Panel with absolute layout
+		// panel with absolute layout
 		JPanel panel = new JPanel();
 		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
 		panel.setLayout(null);
 
-		// Labels and buttons
+		// labels and buttons
 		JLabel label = new JLabel("Select or click on an option:");
 		label.setBounds(10, 10, 200, 20);
 
-		// First column
-		showCashButton.setBounds(10, 40, 220, 50);
-		addProductButton.setBounds(10, 100, 220, 50);
-		addStockButton.setBounds(10, 160, 220, 50);
-		// Second column
-		showInventoryButton.setBounds(255, 40, 220, 50);
-		deleteProductButton.setBounds(255, 100, 220, 50);
-		exitButton.setBounds(255, 160, 220, 50);
+		// first column
+		btnShowCash.setBounds(10, 40, 220, 50);
+		btnAddProduct.setBounds(10, 100, 220, 50);
+		btnAddStock.setBounds(10, 160, 220, 50);
+		btnSetExpired.setBounds(10, 220, 220, 50);
+		btnShowInventory.setBounds(10, 280, 220, 50);
 
-		// Add action listeners for buttons
-		showCashButton.addActionListener(this);
-		addProductButton.addActionListener(this);
-		addStockButton.addActionListener(this);
-		showInventoryButton.addActionListener(this);
-		deleteProductButton.addActionListener(this);
-		exitButton.addActionListener(this);
+		// second column
+		btnMakeSale.setBounds(255, 40, 220, 50);
+		btnShowSale.setBounds(255, 100, 220, 50);
+		btnDeleteProduct.setBounds(255, 160, 220, 50);
+		btnShowTotalSale.setBounds(255, 220, 220, 50);
+		btnExit.setBounds(255, 280, 220, 50);
 
-		// Adding components to the panel
+		// add action listeners for buttons
+		btnShowCash.addActionListener(this);
+		btnAddProduct.addActionListener(this);
+		btnAddStock.addActionListener(this);
+		btnShowInventory.addActionListener(this);
+		btnDeleteProduct.addActionListener(this);
+		btnExit.addActionListener(this);
+
+		// adding components to the panel
 		panel.add(label);
-		panel.add(showCashButton);
-		panel.add(addProductButton);
-		panel.add(addStockButton);
-		panel.add(showInventoryButton);
-		panel.add(deleteProductButton);
-		panel.add(exitButton);
+		panel.add(btnShowCash);
+		panel.add(btnAddProduct);
+		panel.add(btnAddStock);
+		panel.add(btnSetExpired);
+		panel.add(btnShowInventory);
+		panel.add(btnMakeSale);
+		panel.add(btnShowSale);
+		panel.add(btnDeleteProduct);
+		panel.add(btnShowTotalSale);
+		panel.add(btnExit);
 
-		// Adding the panel to the frame
+		// adding the panel to the frame
 		getContentPane().add(panel);
 
 		this.addKeyListener(this);
@@ -231,8 +284,6 @@ public class ShopView extends JFrame implements ActionListener, KeyListener {
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 
 	/**
@@ -254,11 +305,23 @@ public class ShopView extends JFrame implements ActionListener, KeyListener {
 		case KeyEvent.VK_3:
 			openProductView(Constants.ADD_PRODUCT_STOCK);
 			break;
+		case KeyEvent.VK_4:
+			openExpiredView();
+			break;
 		case KeyEvent.VK_5:
 			openInventoryView();
 			break;
-		case KeyEvent.VK_9:
+		case KeyEvent.VK_6:
+			openSaleView();
+			break;
+		case KeyEvent.VK_7:
+			openSaleRecords();
+			break;
+		case KeyEvent.VK_8:
 			openProductView(Constants.DELETE_PRODUCT);
+			break;
+		case KeyEvent.VK_9:
+			openTotalSale();
 			break;
 		case KeyEvent.VK_0:
 			exitOption();
@@ -268,8 +331,6 @@ public class ShopView extends JFrame implements ActionListener, KeyListener {
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
